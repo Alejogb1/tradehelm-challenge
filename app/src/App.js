ab5f22d8ff4146b026550a28afd951bf9c1aa574
Alejogb1 <52103963+Alejogb1@users.noreply.github.com>
2021-03-12 20:30:58 -0300

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react"
+import SocketIO from "socket.io-client";
+const socket =  SocketIO.io("http://localhost:5000")
 
 function App() {
+
+  useEffect(() => {
+    socket.on("state", (state) => console.log(state))
+  }, [])
+
+
   return (
     <>
       
