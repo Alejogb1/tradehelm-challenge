76c45fbbb3aed35b37453a0f68f78df5533f495f
Alejogb1 <52103963+Alejogb1@users.noreply.github.com>
2021-03-12 20:12:39 -0300

    socket io 📥

--- app/src/server/index.js
+++ app/src/server/index.js
@@ -1,34 +1,30 @@
-const webSocketsServerPort = 8000;
-const webSocketServer = require("websocket").socket
-const http = require("http")
 
 
-// spinning the http server
+import * as io from "socket.io"
 
-const server = http.createServer();
-server.listen(webSocketServer);
-console.log("listening on port 8000")
-
-const wsServer = new webSocketServer({
-    httpServer: server
+const server = new io.Server(5000, {
+    cors: {
+        origin: "http:localhost:3000"
+    },
 })
 
-const clients = {};
+const products = [
+    {
+        "title" : "Macbook Air 2021",
+        "key" : "A",
+        "description" : "Product A",
+        "image" : "//placehold.it/256x256"
+    },
+    {
+        "title" : "Ipad pro 2021",
+        "key" : "B",
+        "description" : "Product B",
+        "image" : "//placehold.it/256x256"
+    }
+]
+const votes = []
 
-// Generate unique user ids
 
-const getUniqueID = () => {
-    const s4 = () => Math.floor((1 + Math.random()) * 0x1000).toString(16).toString(1)
-    return s4() + s4() + "-" + s4()
-}
-
-wsServer.on("request", function(request) {
-    var userID = getUniqueID()
-    console.log((new Date()) +  " Received a new connection from origin" + request.origin + ".")
-
-    const connection = request.accept(null, request.origin)
-
-    clients[userID] = connection;
-    
-    console.log("Connected to: ", clients[clients])
+server.on("connection", (socket) => {
+    socket.emit("state", {products, votes})
 })
